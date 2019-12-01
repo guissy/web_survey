f5d377ae5a589f5331f3af620a9d0d8ba75cdf3a
guissy <guissy@qq.com>
2019-12-01 16:30:59 +0800

    Updates

---
+++ precache-manifest.e8622be44706277e68af1c310b2c9149.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f2747fee14a8533f8fe1a31e744248d",
+    "url": "/web_survey/index.html"
+  },
+  {
+    "revision": "009ae84bc2e728031f8f",
+    "url": "/web_survey/static/css/main.175e54b7.chunk.css"
+  },
+  {
+    "revision": "0816e89f93bba8c0bb99",
+    "url": "/web_survey/static/js/2.54eea004.chunk.js"
+  },
+  {
+    "revision": "009ae84bc2e728031f8f",
+    "url": "/web_survey/static/js/main.2a6b1a8c.chunk.js"
+  },
+  {
+    "revision": "b75b87f39f439d54d7b1",
+    "url": "/web_survey/static/js/runtime-main.d503120f.js"
+  }
+]);
